ddd55076efc40f0912501c4e943b8e7faaf6d671
Dominik <44980652+ph342@users.noreply.github.com>
2019-03-10 23:14:56 +0000

    Developments today

--- src/main/java/com/ccomp/model/Item.java
+++ src/main/java/com/ccomp/model/Item.java
@@ -1,5 +1,78 @@
 package com.ccomp.model;
 
-public class Item {
-	//TODO
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Vector;
+
+public final class Item {
+
+	private int item_nr;
+	private String name; // invariant: <=64 chars
+	private String descr; // <=255 chars
+	private double price; // 15,2
+	private String curr; // <=3 chars
+	private int image_key;
+
+	public static Item factory(int item_nr) throws SQLException {
+		try (Connection conn = (Connection) GCloudSQL.createConnectionPool()) {
+			PreparedStatement selectClause = conn
+					.prepareStatement("select * from item where item_nr = " + item_nr + ";");
+			ResultSet result = selectClause.executeQuery();
+			if (result.first())
+				return new Item(result.getInt("item_nr"), result.getString("name"), result.getString("description"),
+						result.getDouble("price"), result.getString("currency"), result.getInt("image_key"));
+		}
+		return null;
+	}
+
+	public static List<Item> loadAllItems() throws SQLException {
+
+		List<Item> ret = new Vector<Item>();
+
+		try (Connection conn = (Connection) GCloudSQL.createConnectionPool()) {
+			PreparedStatement selectClause = conn.prepareStatement("select * from item;");
+			ResultSet result = selectClause.executeQuery();
+			while (result.next()) {
+				ret.add(new Item(result.getInt("item_nr"), result.getString("name"), result.getString("description"),
+						result.getDouble("price"), result.getString("currency"), result.getInt("image_key")));
+			}
+		}
+		return ret;
+	}
+
+	private Item(int item_nr, String name, String descr, double price, String curr, int image_key) {
+		this.item_nr = item_nr;
+		this.name = name;
+		this.descr = descr;
+		this.price = price;
+		this.curr = curr;
+		this.image_key = image_key;
+	}
+
+	public int getItem_nr() {
+		return item_nr;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescr() {
+		return descr;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public String getCurr() {
+		return curr;
+	}
+
+	public int getImage_key() {
+		return image_key;
+	}
 }
